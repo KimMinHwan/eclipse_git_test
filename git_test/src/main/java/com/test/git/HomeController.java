9b029514d77a18161e46d1658c6da87c48e38138
KimMinHwan <laks5318@naver.com>
2017-06-27 10:22:36 +0900

    master branch change

--- git_test/src/main/java/com/test/git/HomeController.java
+++ git_test/src/main/java/com/test/git/HomeController.java
@@ -34,6 +34,7 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		System.out.println("Hellow Git");
+		System.out.println("ekadlarotoRl");
 		
 		return "home";
 	}
